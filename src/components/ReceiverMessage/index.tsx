b62572adec103c441dea55cbc2f9586fdc601c4f
haitran <haitu.dev@gmail.com>
2024-10-04 10:35:29 +0700

    break line message

--- src/components/ReceiverMessage/index.tsx
+++ src/components/ReceiverMessage/index.tsx
@@ -13,11 +13,12 @@ const ReceiverMessage = ({
   avatar,
   contentClassName,
 }: ReceiverMsgProps) => {
+  const breakLine = (msg: string) => msg.replace(/\n/g, "  \n")
   return (
     <div className="flex gap-4">
       <AvatarCustom {...avatar} />
       <p className={twMerge("text-base-md flex-1", contentClassName)}>
-        <Markdown>{content}</Markdown>
+        <Markdown>{breakLine(content)}</Markdown>
       </p>
     </div>
   )
