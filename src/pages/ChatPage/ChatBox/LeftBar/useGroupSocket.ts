8635482b60ce1668c1382d91b510df2d806d716f
haitran <haitu.dev@gmail.com>
2024-10-07 15:03:21 +0700

    update socket group

--- src/pages/ChatPage/ChatBox/LeftBar/useGroupSocket.ts
+++ src/pages/ChatPage/ChatBox/LeftBar/useGroupSocket.ts
@@ -1,18 +1,20 @@
 import useAuthState from "@hooks/useAuthState"
 import { useSocket } from "providers/SocketProvider"
 import { useEffect } from "react"
+import { useParams } from "react-router-dom"
 
 const useGroupSocket = (
   setHasNotiList: React.Dispatch<React.SetStateAction<Array<number>>>,
 ) => {
   const { socket } = useSocket()
   const { user } = useAuthState()
+  const { chatId } = useParams()
 
   useEffect(() => {
     if (socket && user) {
       const event = "chat-group"
       socket.on(event, (e) => {
-        if (e?.user?.id === user?.id) return
+        if (e?.user?.id === user?.id || Number(chatId) === e.group) return
         setHasNotiList((prev) =>
           prev.includes(e.group) ? [...prev] : [...prev, e.group],
         )
@@ -22,7 +24,7 @@ const useGroupSocket = (
         socket.off(event)
       }
     }
-  }, [socket, user])
+  }, [socket, user, chatId])
 }
 
 export default useGroupSocket
