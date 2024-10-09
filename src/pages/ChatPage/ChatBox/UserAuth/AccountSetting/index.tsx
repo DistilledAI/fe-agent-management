4b5b6b2682718eca15c4e7335320d48cb67df8e3
haitran <haitu.dev@gmail.com>
2024-10-09 10:27:37 +0700

    update drawer account

--- src/pages/ChatPage/ChatBox/UserAuth/AccountSetting/index.tsx
+++ src/pages/ChatPage/ChatBox/UserAuth/AccountSetting/index.tsx
@@ -28,71 +28,75 @@ const AccountSetting: React.FC<{
 
   return (
     <DrawerBottom isOpen={isOpen} onClose={onClose}>
-      <div className="text-center text-mercury-900">
-        <p className="mb-1 text-24 font-semibold">My wallet</p>
-        <div className="inline-flex items-center">
-          <div
-            onClick={(e) => copyClipboard(e, user?.publicAddress ?? "")}
-            className="flex cursor-pointer"
-          >
-            <span>Address:</span>
-            <span className="ml-1 font-semibold">
-              {centerTextEllipsis(user?.publicAddress ?? "", 6)}
-            </span>
-          </div>
-          <div
-            onClick={(e) => copyClipboard(e, user?.publicAddress ?? "")}
-            className="ml-3 cursor-pointer"
-          >
-            <CopyIcon />
-          </div>
-          <div className="ml-3">
-            <MetamaskIconSmall />
-          </div>
-          <div
-            onClick={() => {
-              dispatch(logout())
-              navigate(PATH_NAMES.HOME)
-              onClose()
-            }}
-            className="ml-3 cursor-pointer"
-          >
-            <LogoutIcon />
-          </div>
-        </div>
-      </div>
-      <div className="mt-10 flex max-h-[calc(100%-64px)] flex-wrap gap-6 overflow-y-auto pb-[60px] scrollbar-hide">
-        <div className="w-[calc(56%-12px)]">
-          <MyCredits />
-          <div className="mt-4 flex flex-wrap gap-4">
-            <div className="w-[calc(50%-8px)]">
-              <Language />
-            </div>
-            <div className="w-[calc(50%-8px)]">
-              <NotificationSetting />
-            </div>
-          </div>
-          <div className="mt-4 flex flex-wrap gap-4">
-            <div className="w-[calc(60%-8px)]">
-              <AgentCreative />
-            </div>
-            <div className="w-[calc(40%-8px)]">
-              <VoiceChat />
-              <div className="mt-4">
-                <VoiceCharacter />
+      {isOpen && (
+        <>
+          <div className="text-center text-mercury-900">
+            <p className="mb-1 text-24 font-semibold">My wallet</p>
+            <div className="inline-flex items-center">
+              <div
+                onClick={(e) => copyClipboard(e, user?.publicAddress ?? "")}
+                className="flex cursor-pointer"
+              >
+                <span>Address:</span>
+                <span className="ml-1 font-semibold">
+                  {centerTextEllipsis(user?.publicAddress ?? "", 6)}
+                </span>
+              </div>
+              <div
+                onClick={(e) => copyClipboard(e, user?.publicAddress ?? "")}
+                className="ml-3 cursor-pointer"
+              >
+                <CopyIcon />
+              </div>
+              <div className="ml-3">
+                <MetamaskIconSmall />
+              </div>
+              <div
+                onClick={() => {
+                  dispatch(logout())
+                  navigate(PATH_NAMES.HOME)
+                  onClose()
+                }}
+                className="ml-3 cursor-pointer"
+              >
+                <LogoutIcon />
               </div>
             </div>
           </div>
-        </div>
-        <div className="w-[calc(44%-12px)]">
-          <div>
-            <PrivateAgent />
+          <div className="mt-10 flex max-h-[calc(100%-64px)] flex-wrap gap-6 overflow-y-auto pb-[60px] scrollbar-hide">
+            <div className="w-[calc(56%-12px)]">
+              <MyCredits />
+              <div className="mt-4 flex flex-wrap gap-4">
+                <div className="w-[calc(50%-8px)]">
+                  <Language />
+                </div>
+                <div className="w-[calc(50%-8px)]">
+                  <NotificationSetting />
+                </div>
+              </div>
+              <div className="mt-4 flex flex-wrap gap-4">
+                <div className="w-[calc(60%-8px)]">
+                  <AgentCreative />
+                </div>
+                <div className="w-[calc(40%-8px)]">
+                  <VoiceChat />
+                  <div className="mt-4">
+                    <VoiceCharacter />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-[calc(44%-12px)]">
+              <div>
+                <PrivateAgent />
+              </div>
+              <div className="mt-4">
+                <PrivateAgentPod />
+              </div>
+            </div>
           </div>
-          <div className="mt-4">
-            <PrivateAgentPod />
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </DrawerBottom>
   )
 }
