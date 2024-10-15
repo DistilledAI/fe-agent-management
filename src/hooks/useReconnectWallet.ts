d67b9f67b9fdbb3f883d8484fdf7db49c94108f9
haitran <haitu.dev@gmail.com>
2024-10-15 09:50:49 +0700

    logout when change account

--- src/hooks/useReconnectWallet.ts
+++ src/hooks/useReconnectWallet.ts
@@ -1,12 +1,10 @@
 import { useEffect } from "react"
 import useAuthState from "./useAuthState"
-import useConnectWallet from "./useConnectWallet"
 import { useDispatch } from "react-redux"
 import { logout } from "@reducers/user/UserSlice"
 
 const useReconnectWallet = () => {
   const { isLogin, user } = useAuthState()
-  const { connectWallet } = useConnectWallet()
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -16,7 +14,6 @@ const useReconnectWallet = () => {
       const isReconnect = user && user.publicAddress !== accounts[0]
       if (isReconnect) {
         dispatch(logout())
-        connectWallet()
       }
     }
     window.ethereum.on("accountsChanged", handleAccountsChanged)
@@ -26,7 +23,7 @@ const useReconnectWallet = () => {
         window.ethereum.removeListener("accountsChanged", handleAccountsChanged)
       }
     }
-  }, [isLogin, connectWallet, user, dispatch])
+  }, [isLogin, user, dispatch])
 }
 
 export default useReconnectWallet
