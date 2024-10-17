98d3bf95e63972af4fdf35f403eb099ccc1c8b1c
haitran <haitu.dev@gmail.com>
2024-10-17 16:04:17 +0700

    update chat mobile

--- src/pages/ChatPage/Mobile/index.tsx
+++ src/pages/ChatPage/Mobile/index.tsx
@@ -1,7 +1,15 @@
+import ChatList from "./ChatList"
+import ChatSearch from "./ChatSearch"
+
 const ChatPageMobile = () => {
   return (
     <div className="min-h-[calc(100dvh-110px)] bg-mercury-30 font-barlow">
-      Chat PAGE
+      <div className="fixed left-0 top-[50px] w-full bg-[rgba(255,255,255,0.85)] px-4 py-1 backdrop-blur-[10px]">
+        <ChatSearch />
+      </div>
+      <div className="h-[calc(100vh-110px)] w-full bg-[rgba(255,255,255,0.85)] pt-[56px]">
+        <ChatList />
+      </div>
     </div>
   )
 }
