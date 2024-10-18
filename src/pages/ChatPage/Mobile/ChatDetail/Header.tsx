47208031d0ee5958105bdd86bb23b01470c41b48
haitran <haitu.dev@gmail.com>
2024-10-18 12:05:13 +0700

    search mobile chat

--- src/pages/ChatPage/Mobile/ChatDetail/Header.tsx
+++ src/pages/ChatPage/Mobile/ChatDetail/Header.tsx
@@ -1,17 +1,22 @@
-// import AvatarCustom from "@components/AvatarCustom"
-// import AvatarGroup from "@components/AvatarGroup"
+import AvatarCustom from "@components/AvatarCustom"
+import AvatarGroup from "@components/AvatarGroup"
 import MoreAction from "@components/ChatWindow/MoreAction"
 import { ArrowLeftFilledIcon } from "@components/Icons/Arrow"
 import { Button } from "@nextui-org/react"
-// import { TypeGroup } from "@pages/ChatPage/ChatBox/LeftBar/useFetchGroups"
-// import { useChatMessage } from "providers/MessageProvider"
+import { TypeGroup } from "@pages/ChatPage/ChatBox/LeftBar/useFetchGroups"
 import { useNavigate } from "react-router-dom"
+import useFetchDetail from "./useFetch"
+import {
+  getAvatarGroupChat,
+  getNameGroup,
+} from "@pages/ChatPage/ChatBox/LeftBar/helpers"
+import useAuthState from "@hooks/useAuthState"
 
 const ChatDetailHeader = () => {
   const navigate = useNavigate()
-  // const { dataFetch } = useChatMessage()
-  // const firstMessage = dataFetch[0]
-  // const isGroup = firstMessage?.group?.typeGroup === TypeGroup.PRIVATE_GROUP
+  const { user } = useAuthState()
+  const { groupDetail } = useFetchDetail()
+  const isGroup = groupDetail?.group?.typeGroup === TypeGroup.PRIVATE_GROUP
 
   return (
     <div className="fixed left-0 top-0 z-[1] flex h-[55px] w-full items-center justify-between bg-white px-4">
@@ -22,16 +27,31 @@ const ChatDetailHeader = () => {
         >
           <ArrowLeftFilledIcon />
         </Button>
-        {/* <div>
-          {isGroup ? (
-            <AvatarGroup groupName={firstMessage?.group?.name} />
-          ) : (
-            <div className="flex items-center gap-2">
-              <AvatarCustom />
-              <span className="font-semibold">Distill AI</span>
-            </div>
-          )}
-        </div> */}
+        {groupDetail && (
+          <div>
+            {isGroup ? (
+              <AvatarGroup groupName={groupDetail.group.name} />
+            ) : (
+              <div className="flex items-center gap-2">
+                <AvatarCustom
+                  src={getAvatarGroupChat(
+                    groupDetail.userId,
+                    groupDetail.group.userA,
+                    groupDetail.group.userB,
+                  )}
+                  classNames={{ base: "w-[36px] h-[36px]" }}
+                />
+                <span className="line-clamp-1 max-w-[180px] text-15 font-semibold">
+                  {getNameGroup(
+                    user,
+                    groupDetail.group.userA,
+                    groupDetail.group.userB,
+                  )}
+                </span>
+              </div>
+            )}
+          </div>
+        )}
       </div>
       <div>
         <MoreAction />
