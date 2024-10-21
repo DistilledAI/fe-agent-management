8a2680146172ce7f546035acf2df85c38d3b26f8
huynhtd <huynhtran924@gmail.com>
2024-10-21 11:40:43 +0700

    Done convert state messages to query messages

--- src/pages/ChatPage/index.tsx
+++ src/pages/ChatPage/index.tsx
@@ -2,7 +2,6 @@ import MainLayout from "@components/Layout/MainLayout"
 import { PATH_NAMES } from "@constants/index"
 import useInviteUser from "@hooks/useInviteUser"
 import useWindowSize from "@hooks/useWindowSize"
-import { ChatMessageProvider } from "providers/MessageProvider"
 import { useLocation, useParams } from "react-router-dom"
 import ChatBox from "./ChatBox"
 import ChatHeader from "./ChatHeader"
@@ -19,16 +18,12 @@ const ChatPage = () => {
   return (
     <MainLayout isHeader={hasLayout} isFooter={hasLayout}>
       {isMobile ? (
-        <ChatMessageProvider>
-          <ChatPageMobile />
-        </ChatMessageProvider>
+        <ChatPageMobile />
       ) : (
         <div className="bg-white font-barlow">
           <ChatHeader />
           <div className="mx-auto h-[calc(100dvh-54px)] w-full max-w-[calc(100%-30px)]">
-            <ChatMessageProvider>
-              <ChatBox />
-            </ChatMessageProvider>
+            <ChatBox />
           </div>
         </div>
       )}
