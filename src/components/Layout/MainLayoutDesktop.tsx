494f0bd718e01029b4ccde2e93a4f63b325be003
huynhtd <huynhtran924@gmail.com>
2024-10-24 11:27:47 +0700

    Done update new layout marketplace desktop

--- src/components/Layout/MainLayoutDesktop.tsx
+++ src/components/Layout/MainLayoutDesktop.tsx
@@ -4,8 +4,11 @@ import useFetchMe from "@hooks/useFetchMe"
 import { StyleBoxChatProvider } from "@pages/ChatPage/ChatBox/StyleProvider"
 import LeftBar from "@pages/ChatPage/ChatBox/LeftBar"
 import UserAuthWrapper from "@pages/ChatPage/ChatBox/UserAuth/UserAuthWrapper"
+import { useAppSelector } from "@hooks/useAppRedux"
+import { twMerge } from "tailwind-merge"
 
 const MainLayoutDesktop = () => {
+  const sidebarCollapsed = useAppSelector((state) => state.sidebarCollapsed)
   useInviteUser()
   useFetchMe()
 
@@ -14,7 +17,12 @@ const MainLayoutDesktop = () => {
       <div className="flex">
         <StyleBoxChatProvider>
           <LeftBar />
-          <div className="min-h-dvh w-full flex-1 pt-[68px]">
+          <div
+            className={twMerge(
+              "min-h-dvh w-[calc(100%-329px)] pt-[68px] transition-all duration-500 ease-linear",
+              sidebarCollapsed && "w-[calc(100%-104px)]",
+            )}
+          >
             <UserAuthWrapper />
             <Outlet />
           </div>
--- src/pages/Marketplace/index.tsx
+++ src/pages/Marketplace/index.tsx
@@ -1,35 +1,55 @@
-import { FilledSquareCircleIcon } from "@components/Icons/FilledSquareCircleIcon"
-import { PATH_NAMES } from "@constants/index"
-import useWindowSize from "@hooks/useWindowSize"
-import { Button, useDisclosure } from "@nextui-org/react"
-import { useEffect } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
-import MarketplaceDesktop from "./Desktop"
+import { useEffect, useRef, useState } from "react"
 import GenAITools from "./GenAITools"
-import MarketplaceMobile from "./Mobile"
 import PrivateAgents from "./PrivateAgents"
 import Productivity from "./Productivity"
-import { useAppSelector } from "@hooks/useAppRedux"
 import { twMerge } from "tailwind-merge"
+import ComingSoon from "@components/ComingSoon"
 
 const Marketplace = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const { isMobile } = useWindowSize()
-  const navigate = useNavigate()
-  const location = useLocation()
-  const sidebarCollapsed = useAppSelector((state) => state.sidebarCollapsed)
+  const containerRef = useRef<HTMLDivElement>(null)
+  const sectionsRefs = useRef<(HTMLDivElement | null)[]>([])
+  const [activeCategoryIndex, setActiveCategoryIndex] = useState(0)
+  const [isNavAction, setIsNavAction] = useState(false)
 
   useEffect(() => {
-    if (!isMobile && location.pathname === PATH_NAMES.MARKETPLACE) {
-      navigate(PATH_NAMES.HOME)
+    const handleScroll = () => {
+      if (isNavAction) return
+      sectionsRefs.current.forEach((section, index) => {
+        if (section) {
+          const rect = section.getBoundingClientRect()
+          const inView =
+            rect.top >= 0 && rect.bottom <= window.innerHeight - 200
+          if (inView) {
+            setActiveCategoryIndex(index)
+          }
+        }
+      })
     }
-  }, [isMobile, location.pathname])
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [isNavAction])
+
+  const scrollToSection = (index: number) => {
+    setIsNavAction(true)
+    setTimeout(() => {
+      setIsNavAction(false)
+    }, 2000)
+    setActiveCategoryIndex(index)
+    sectionsRefs.current[index]?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    })
+  }
 
   const CATEGORIES = [
     {
       key: "private-agents",
       name: "Private agents",
-      component: <PrivateAgents onClose={onClose} />,
+      component: <PrivateAgents />,
       isComing: false,
     },
     {
@@ -47,75 +67,113 @@ const Marketplace = () => {
     {
       key: "learning",
       name: "Learning",
-      component: <div className="text-base-md">Coming soon...</div>,
+      component: <div className="text-base-md h-[300px]">Coming soon...</div>,
       isComing: false,
     },
     {
       key: "wellness",
       name: "Wellness",
-      component: <div className="text-base-md">Coming soon...</div>,
+      component: <div className="text-base-md h-[300px]">Coming soon...</div>,
       isComing: false,
     },
     {
       key: "search-engine",
       name: "Search engine",
-      component: <div className="text-base-md">Coming soon...</div>,
+      component: <div className="text-base-md h-[300px]">Coming soon...</div>,
       isComing: false,
     },
     {
       key: "foundational-access",
       name: "Foundational access",
-      component: <div className="text-base-md">Coming soon...</div>,
+      component: <div className="text-base-md h-[300px]">Coming soon...</div>,
       isComing: false,
     },
     {
       key: "sns",
       name: "SNS",
-      component: <div className="text-base-md">Coming soon...</div>,
+      component: <div className="text-base-md h-[300px]">Coming soon...</div>,
       isComing: false,
     },
     {
       key: "finance",
       name: "Finance",
-      component: <div className="text-base-md">Coming soon...</div>,
+      component: <div className="text-base-md h-[300px]">Coming soon...</div>,
       isComing: false,
     },
     {
       key: "characters-org",
       name: "Characters & Org",
-      component: <div className="text-base-md">Coming soon...</div>,
+      component: <div className="text-base-md h-[300px]">Coming soon...</div>,
       isComing: false,
     },
   ]
 
-  return isMobile ? (
-    <MarketplaceMobile categories={CATEGORIES} />
-  ) : (
+  return (
     <>
-      <Button
-        onClick={onOpen}
-        className={twMerge(
-          "btn-primary min-h-[60px]",
-          sidebarCollapsed && "min-h-14 min-w-14",
-        )}
-      >
-        <div>
-          <FilledSquareCircleIcon />
-        </div>
-        <span
-          className={twMerge(
-            "text-[16px] text-mercury-900",
-            sidebarCollapsed && "hidden",
-          )}
-        >
+      <div className="sticky top-[68px] z-10 space-y-4 bg-white pb-4 pt-2">
+        <h3 className="text-center text-24 font-semibold text-mercury-950">
           Marketplace
-        </span>
-      </Button>
-      <MarketplaceDesktop
-        categories={CATEGORIES}
-        isOpen={isOpen}
-        onClose={onClose}
-      />
+        </h3>
+        <div
+          className="fixed z-10 flex items-center gap-3 overflow-hidden overflow-x-scroll whitespace-nowrap bg-white/85 px-4 backdrop-blur-[10px] scrollbar-hide md:static"
+          ref={containerRef}
+        >
+          {CATEGORIES.map((category, index) => (
+            <button
+              type="button"
+              key={category.key}
+              id={`category-button-${index}`}
+              className={twMerge(
+                "h-11 flex-shrink-0 rounded-full bg-white px-4 text-[16px] text-mercury-900 transition-all duration-500 ease-in-out",
+                activeCategoryIndex === index && "bg-mercury-900 text-white",
+              )}
+              onClick={() => scrollToSection(index)}
+            >
+              {category.name}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <div className="mx-auto flex h-full w-full max-w-[768px] flex-col gap-y-6 overflow-hidden px-4 max-md:pb-20 md:items-center">
+        {CATEGORIES.map((category, index) => (
+          <div
+            className="w-full space-y-3 transition-all duration-500 ease-in-out"
+            id={`category-section-${index}`}
+            key={`${category.key}-${index}`}
+            ref={(el) => (sectionsRefs.current[index] = el)}
+          >
+            <h5
+              className={twMerge(
+                "text-[18px] text-mercury-900",
+                activeCategoryIndex === index && "font-medium text-mercury-950",
+              )}
+            >
+              {category.name}
+            </h5>
+            <div
+              className={twMerge(
+                "grid grid-cols-2 justify-between gap-x-20 gap-y-6 overflow-y-auto rounded-[22px] bg-mercury-30 px-1 py-3 md:px-2 md:py-4",
+                category.isComing && "overflow-y-visible",
+              )}
+            >
+              {category.isComing ? (
+                <ComingSoon wrapperClassName="col-span-2">
+                  <div
+                    className={twMerge(
+                      category.isComing && "space-y-3 px-2 opacity-60",
+                    )}
+                  >
+                    {category.component}
+                  </div>
+                </ComingSoon>
+              ) : (
+                category.component
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
