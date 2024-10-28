2ed2e148f232b4cb30e690d70ad419cff38e8a8a
haitran <haitu.dev@gmail.com>
2024-10-28 17:09:25 +0700

    warning allow microphone

--- src/pages/ChatPage/ChatBox/ChatInput/Voice.tsx
+++ src/pages/ChatPage/ChatBox/ChatInput/Voice.tsx
@@ -1,8 +1,12 @@
+import { allowMicrophone } from "@assets/images"
+import DrawerBottom from "@components/DrawerBottom"
 import { MicrophoneFilledIcon } from "@components/Icons/Microphone"
-import { Button } from "@nextui-org/react"
-import React, { useEffect } from "react"
+import useWindowSize from "@hooks/useWindowSize"
+import { Button, Image } from "@nextui-org/react"
+import React, { useEffect, useState } from "react"
 import type SpeechRecognition from "react-speech-recognition"
 import { toast } from "react-toastify"
+import { match } from "ts-pattern"
 
 const VoiceChat: React.FC<{
   setMessages: React.Dispatch<React.SetStateAction<string>>
@@ -19,16 +23,48 @@ const VoiceChat: React.FC<{
   resetTranscript,
   isDisabled,
 }) => {
-  const startVoice = () => {
-    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-      return toast.warning("Your browser is not support")
-    }
+  const [isOpen, setIsOpen] = useState(false)
+  const { isMobile } = useWindowSize()
+
+  const startVoice = async () => {
+    const isMicrophoneAllowed = await isMicrophoneAllow()
+    const isBrowserSupport =
+      SpeechRecognition.browserSupportsSpeechRecognition()
+
+    const isPassRule = match({
+      isMicrophoneAllowed,
+      isMobile,
+      isBrowserSupport,
+    })
+      .returnType<boolean>()
+      .with({ isMicrophoneAllowed: false, isMobile: false }, () => {
+        toast.warning("Please allow microphone access")
+        return false
+      })
+      .with({ isMicrophoneAllowed: false, isMobile: true }, () => {
+        setIsOpen(true)
+        return false
+      })
+      .with({ isBrowserSupport: false }, () => {
+        toast.warning("Your browser is not support")
+        return false
+      })
+      .otherwise(() => true)
+    if (!isPassRule) return
+
     resetTranscript()
     SpeechRecognition.startListening({ continuous: true })
   }
 
   const stopVoice = () => {
     SpeechRecognition.stopListening()
+  }
+
+  const isMicrophoneAllow = async () => {
+    const permissionNav: any = navigator.permissions
+    const permission = await permissionNav.query({ name: "microphone" })
+    if (permission.state === "granted") return true
+    return false
   }
 
   useEffect(() => {
@@ -59,6 +95,34 @@ const VoiceChat: React.FC<{
         >
           <MicrophoneFilledIcon />
         </Button>
+      )}
+      {isMobile && (
+        <>
+          {isOpen && (
+            <div className="fixed left-0 top-0 h-dvh w-full bg-[rgba(0,0,0,0.25)] backdrop-blur-[10px]"></div>
+          )}
+          <DrawerBottom
+            classNames={{
+              base: "max-h-[calc(100dvh-200px)] bg-mercury-30 rounded-b-none",
+            }}
+            isOpen={isOpen}
+            onClose={() => setIsOpen(false)}
+          >
+            <div className="flex h-full w-full flex-col justify-center overflow-y-auto">
+              <Image className="mt-6 h-auto w-full" src={allowMicrophone} />
+              <p className="mt-5 text-center text-24 text-base font-semibold">
+                Allow Microphone access
+              </p>
+              <p className="mt-6 text-center !text-[14px] text-base text-mercury-800">
+                <span className="!text-[14px] text-base font-medium">
+                  Voice chat with AI Companion
+                </span>{" "}
+                requires Microphone access for receiving and processing outgoing
+                audio
+              </p>
+            </div>
+          </DrawerBottom>
+        </>
       )}
     </>
   )
