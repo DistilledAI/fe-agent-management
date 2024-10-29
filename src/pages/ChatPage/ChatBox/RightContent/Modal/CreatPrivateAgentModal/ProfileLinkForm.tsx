194689ebb00ded7d1799ec6b35b2ff02d0c3678b
thuongdo <thuong.dn@orai.io>
2024-10-29 14:23:19 +0700

    hot fix craw x profile link

--- src/pages/ChatPage/ChatBox/RightContent/Modal/CreatPrivateAgentModal/ProfileLinkForm.tsx
+++ src/pages/ChatPage/ChatBox/RightContent/Modal/CreatPrivateAgentModal/ProfileLinkForm.tsx
@@ -63,9 +63,10 @@ const ProfileLinkForm: React.FC<{
     const userName = getUserName(profileLink) as any
 
     try {
-      const typeValue = selectedKey === PROFILE_TYPE.TWITTER ? "x" : selectedKey
+      const typePayload =
+        selectedKey === PROFILE_TYPE.TWITTER ? "x" : selectedKey
       const res = await getProfileInfo({
-        type: typeValue,
+        type: typePayload,
         userName,
       })
       if (res) {
