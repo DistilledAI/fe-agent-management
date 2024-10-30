f9124fd6a0a75f31b68af9d9f572267051c05a3d
thuongdo <thuong.dn@orai.io>
2024-10-30 09:24:19 +0700

    hide clear context action

--- src/pages/ChatPage/ChatBox/ChatMessages/ChatActions/index.tsx
+++ src/pages/ChatPage/ChatBox/ChatMessages/ChatActions/index.tsx
@@ -1,10 +1,9 @@
 import DelegatePrivateAgent from "./DelegatePrivateAgent"
-import ClearChat from "./ClearChat"
 
 const ChatActions = () => {
   return (
     <div className="absolute bottom-[69px] left-1/2 z-10 mx-auto flex w-full max-w-[768px] -translate-x-1/2 items-center justify-between bg-mercury-30 px-3 py-2 md:bottom-[96px] md:bg-white lg:px-0">
-      <ClearChat />
+      {/* <ClearChat /> */}
       <div className="hidden md:block">
         <DelegatePrivateAgent />
       </div>
