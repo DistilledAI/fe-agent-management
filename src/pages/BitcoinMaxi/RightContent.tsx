9fa96de3972f4eba71162b4f1afa02fbc0fe037b
haitran <haitu.dev@gmail.com>
2024-10-31 17:08:59 +0700

    responsive live chat

--- src/pages/BitcoinMaxi/RightContent.tsx
+++ src/pages/BitcoinMaxi/RightContent.tsx
@@ -2,8 +2,11 @@ import ChatWindow from "@components/ChatWindow"
 import MessageLive from "./MessageLive"
 import ChatInput from "@pages/ChatPage/ChatBox/ChatInput"
 import { twMerge } from "tailwind-merge"
+import { useAppSelector } from "@hooks/useAppRedux"
 
 const RightContent: React.FC = () => {
+  const sidebarCollapsed = useAppSelector((state) => state.sidebarCollapsed)
+
   const messages: any = [
     {},
     {},
@@ -39,7 +42,7 @@ const RightContent: React.FC = () => {
   const onSubmit = () => {}
 
   return (
-    <div className="flex-1 px-10 max-2xl:px-0 max-lg:flex-none">
+    <div className="flex-1 px-10 max-2xl:px-0 max-lg:h-[300px] max-lg:flex-none max-md:h-[250px]">
       <ChatWindow
         messages={messages}
         itemContent={renderMessage}
@@ -51,9 +54,14 @@ const RightContent: React.FC = () => {
         chatId={"1"}
         msgBoxClassName="p-0 md:px-4"
         isChatAction={false}
-        className="md:max-h-[calc(100%-80px)]"
+        className="max-lg:!max-h-full md:max-h-[calc(100%-80px)]"
       />
-      <div className="relative bg-white pt-3 max-lg:fixed max-lg:bottom-0 max-lg:left-[121px] max-lg:right-4 max-lg:pb-4">
+      <div
+        className={twMerge(
+          "relative bg-white pt-3 duration-300 max-lg:fixed max-lg:bottom-0 max-lg:left-[344px] max-lg:right-4 max-lg:pb-4 max-md:bottom-0 max-md:left-0 max-md:right-0 max-md:px-4",
+          sidebarCollapsed && "max-lg:left-[121px]",
+        )}
+      >
         <div className="absolute bottom-[calc(100%-5px)] h-6 w-full bg-fading-white"></div>
         <ChatInput
           onSubmit={onSubmit}
