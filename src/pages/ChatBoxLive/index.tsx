19a6770c206cd517496fbf0055e83abe5448e88b
huynhtd <huynhtran924@gmail.com>
2024-11-06 15:34:16 +0700

    Done add toggle actions live chat frame mobile

--- src/pages/ChatBoxLive/index.tsx
+++ src/pages/ChatBoxLive/index.tsx
@@ -7,11 +7,11 @@ const ChatBoxLive = () => {
   useJoinGroupLive()
 
   return (
-    <div className="relative mx-auto h-dvh max-w-[1232px] overflow-hidden max-md:pt-[60px] md:h-[calc(100dvh-68px)] md:px-6">
+    <div className="relative mx-auto h-dvh max-w-[1232px] overflow-hidden max-md:pt-[58px] md:h-[calc(100dvh-68px)] md:px-6">
       <div className="md:hidden">
         <ChatLiveHeader />
       </div>
-      <div className="flex h-full max-h-dvh gap-2 pb-4 max-lg:flex-col md:gap-10">
+      <div className="flex h-full gap-2 pb-4 max-lg:flex-col md:gap-10">
         <LeftContent />
         <RightContent />
       </div>
