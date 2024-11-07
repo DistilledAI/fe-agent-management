aec4e2fd8743fcdd8c0ad147591ca5efa2a6b260
haitran <haitu.dev@gmail.com>
2024-11-07 15:30:55 +0700

    replace span to input - edit profile

--- src/pages/Account/Profile/Username.tsx
+++ src/pages/Account/Profile/Username.tsx
@@ -2,7 +2,7 @@ import { EditPenFilledIcon } from "@components/Icons/Edit"
 import useAuthState from "@hooks/useAuthState"
 import { Button } from "@nextui-org/react"
 import { IUser, updateUser as updateUserSlice } from "@reducers/userSlice"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useDispatch } from "react-redux"
 import { toast } from "react-toastify"
 import { updateUser } from "services/user"
@@ -11,13 +11,18 @@ const AuthorUsername = () => {
   const { user } = useAuthState()
   const dispatch = useDispatch()
   const nameRef = useRef<any>()
+  const [value, setValue] = useState(user?.username)
+
+  useEffect(() => {
+    setValue(user?.username)
+  }, [user?.username])
 
   const handleUpdate = async () => {
-    const username = nameRef.current.innerText
-    if (username === user?.username) return
-    dispatch(updateUserSlice({ user: { ...user, username } as IUser }))
+    if (value?.trim() === "") return setValue(user?.username)
+    if (value === user?.username) return
+    dispatch(updateUserSlice({ user: { ...user, username: value } as IUser }))
     await updateUser({
-      username,
+      username: value,
       description: user?.description,
       avatar: user?.avatar,
     })
@@ -26,13 +31,6 @@ const AuthorUsername = () => {
 
   const handleFocus = () => {
     nameRef.current.focus()
-    const range = document.createRange()
-    const selection = window.getSelection()
-    if (!selection) return
-    range.selectNodeContents(nameRef.current)
-    range.collapse(false)
-    selection.removeAllRanges()
-    selection.addRange(range)
   }
 
   const handleKeyDown = (e: any) => {
@@ -42,19 +40,22 @@ const AuthorUsername = () => {
     }
   }
 
+  const handleOnChange = (value: string) => {
+    setValue(value)
+  }
+
   return (
     <div className="flex items-center justify-between">
       <span className="text-mercury-600">Name:</span>
       <div className="flex items-center gap-2">
-        <span
+        <input
+          className="max-w-36 border-none bg-transparent text-right shadow-none outline-none"
+          value={value}
+          onChange={(e) => handleOnChange(e.target.value)}
+          onKeyDown={handleKeyDown}
           ref={nameRef}
           onBlur={handleUpdate}
-          onKeyDown={handleKeyDown}
-          contentEditable={true}
-          className="line-clamp-1 block max-w-36 text-ellipsis whitespace-nowrap text-mercury-900 focus:border-none focus:outline-none"
-        >
-          {user?.username}
-        </span>
+        />
         <Button
           onClick={handleFocus}
           className="h-auto w-auto min-w-0 bg-transparent p-0"
