fe129626a4508b0f8d26ce02ad19b257208cae71
huynhtd <huynhtran924@gmail.com>
2024-11-07 10:59:01 +0700

    Done update button link X agent MAX

--- src/pages/ChatBoxLive/TwitterButton.tsx
+++ src/pages/ChatBoxLive/TwitterButton.tsx
@@ -1,15 +1,14 @@
 import { TwitterIcon } from "@components/Icons/Twitter"
 import { Button } from "@nextui-org/react"
+import { Link } from "react-router-dom"
 
 const TwitterButton = () => {
-  const openXLink = () => {
-    window.open("https://x.com/maxisbuyin", "_blank")
-  }
-
   return (
     <Button
+      as={Link}
+      to="https://x.com/maxisbuyin"
+      target="_blank"
       className="h-14 w-full rounded-full bg-mercury-70 text-white md:h-11"
-      onClick={openXLink}
     >
       <TwitterIcon />
       <span className="text-base text-mercury-900">Twitter (AI)</span>
