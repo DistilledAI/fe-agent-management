f7eb3d01578aa04675f1cd8cba4a96e16ce82844
huynhtd <huynhtran924@gmail.com>
2024-11-07 18:14:04 +0700

    Done feat refetch messages focus, online

--- src/pages/ChatBoxLive/index.tsx
+++ src/pages/ChatBoxLive/index.tsx
@@ -9,7 +9,7 @@ const ChatBoxLive = () => {
   const { isMobile } = useWindowSize()
 
   return (
-    <div className="relative mx-auto h-dvh max-w-[1232px] bg-mercury-30 max-md:overflow-hidden max-md:pt-[68px] md:h-[calc(100dvh-68px)] md:bg-white md:px-6">
+    <div className="relative mx-auto h-dvh max-w-[1232px] bg-mercury-30 max-md:overflow-hidden max-md:pt-[60px] md:h-[calc(100dvh-68px)] md:bg-white md:px-6">
       {isMobile ? <ChatLiveHeader /> : <></>}
 
       <div className="flex h-full gap-2 pb-4 max-lg:flex-col md:gap-10">
