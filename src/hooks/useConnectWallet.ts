98deb7aa50e0d99156f1de4410e42331aaa6938d
thuongdo <thuong.dn@orai.io>
2024-11-13 10:56:48 +0700

    test connect owallet

--- src/hooks/useConnectWallet.ts
+++ src/hooks/useConnectWallet.ts
@@ -55,20 +55,21 @@ const useConnectWallet = () => {
 
   const connectWallet = async () => {
     const isOwallet = window.ethereum.isOwallet
-    if (!window.ethereum || !window.ethereum.isMetaMask || !isOwallet) {
-      if (isMobile) {
-        toast.info("Please open the application in metamask's browser")
-        setTimeout(() => {
-          window.open(
-            `https://metamask.app.link/dapp/${document.location.host}`,
-            "_blank",
-          )
-        }, 1000)
-        return
-      }
-      toast.warning("Please install MetaMask to continue!")
-      return
-    }
+    // if (!window.ethereum || !window.ethereum.isMetaMask || !isOwallet) {
+    //   if (isMobile) {
+    //     toast.info("Please open the application in metamask's browser")
+    //     setTimeout(() => {
+    //       window.open(
+    //         `https://metamask.app.link/dapp/${document.location.host}`,
+    //         "_blank",
+    //       )
+    //     }, 1000)
+    //     return
+    //   }
+
+    //   toast.warning("Please install MetaMask to continue!")
+    //   return
+    // }
 
     const isTrustWalletDefault =
       window.ethereum.isTrust || window.ethereum.isTrustWallet
