7fa6257293cf746af2982c0d06b43eadb100426e
haitran <haitu.dev@gmail.com>
2024-11-14 18:30:44 +0700

    check created bot

--- src/pages/ChatPage/ChatBox/RightContent/MyPrivateAgentContent/AgentInitialization/Header.tsx
+++ src/pages/ChatPage/ChatBox/RightContent/MyPrivateAgentContent/AgentInitialization/Header.tsx
@@ -12,7 +12,7 @@ const Header = ({ isLoading }: Props) => {
   return (
     <div className="sticky top-[50px] z-10 flex items-center justify-center bg-lgd-muted-beige-2 p-3 backdrop-blur-3xl md:top-[68px]">
       <div className="flex w-full max-w-[768px] gap-2 max-md:flex-col md:items-center md:justify-between">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center md:items-start">
           <span className="text-24 font-semibold text-mercury-950">
             Agent Initialization
           </span>
