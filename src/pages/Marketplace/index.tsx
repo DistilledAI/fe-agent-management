3b48256b9bc6b86d830652edc3775a7edd7592c4
huynhtd <huynhtran924@gmail.com>
2024-11-20 18:03:12 +0700

    Done update padding title categories marketplace

--- src/pages/Marketplace/index.tsx
+++ src/pages/Marketplace/index.tsx
@@ -155,7 +155,7 @@ const Marketplace = () => {
         </ScrollShadow>
       </div>
 
-      <div className="mx-auto flex h-full w-full max-w-[768px] flex-col gap-y-6 overflow-hidden bg-mercury-70 px-4 pb-20 pt-[100px] md:items-center md:bg-white md:pb-4 md:pt-[116px] 2xl:max-w-[940px]">
+      <div className="mx-auto flex h-full w-full max-w-[768px] flex-col gap-y-6 overflow-hidden bg-mercury-70 px-4 pb-20 pt-[140px] md:items-center md:bg-white md:pb-4 md:pt-[124px] 2xl:max-w-[940px]">
         {CATEGORIES.map((category, index) => (
           <div
             className="w-full space-y-3 transition-all duration-500 ease-in-out"
