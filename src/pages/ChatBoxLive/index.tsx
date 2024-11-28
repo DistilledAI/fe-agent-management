6403504e0acda855a47390617863bf3250e13dcd
haitran <haitu.dev@gmail.com>
2024-11-28 17:27:27 +0700

    hiden video live when not maxibuyin

--- src/pages/ChatBoxLive/index.tsx
+++ src/pages/ChatBoxLive/index.tsx
@@ -3,17 +3,26 @@ import useWindowSize from "@hooks/useWindowSize"
 import ChatLiveHeader from "./ChatLiveHeader"
 import LeftContent from "./LeftContent"
 import RightContent from "./RightContent"
+import { useParams } from "react-router-dom"
+import { twMerge } from "tailwind-merge"
 
 const ChatBoxLive = () => {
   useJoinGroupLive()
   const { isMobile } = useWindowSize()
+  const { chatId } = useParams()
+  const isMaxi = chatId === "@maxisbuyin"
 
   return (
-    <div className="relative mx-auto h-dvh max-w-[1232px] bg-mercury-30 max-md:overflow-hidden max-md:pt-[60px] md:h-[calc(100dvh-68px)] md:bg-white md:px-6">
+    <div
+      className={twMerge(
+        "relative mx-auto h-dvh max-w-[1232px] bg-mercury-30 max-md:overflow-hidden max-md:pt-[60px] md:h-[calc(100dvh-68px)] md:bg-white md:px-6",
+        !isMaxi && "max-w-[768px]",
+      )}
+    >
       {isMobile ? <ChatLiveHeader /> : <></>}
 
       <div className="flex h-full gap-2 pb-4 max-lg:flex-col md:gap-10">
-        <LeftContent />
+        {isMaxi && <LeftContent />}
         <RightContent isClan />
       </div>
     </div>
