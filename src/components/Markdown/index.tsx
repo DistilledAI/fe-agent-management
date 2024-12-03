486f3d772714279e9c666c62e6d0655ef06aa834
thuongdo <thuong.dn@orai.io>
2024-12-03 17:27:23 +0700

    update: remove \n

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -22,9 +22,11 @@ const MarkdownMessage = ({ msg }: { msg: string }) => {
   }
 
   const breakLine = (text: string) => {
-    let md = text
+    const newText = text.replace(/\n+$/, "")
+
+    let md = newText
     // Support multiple linebreaks
-    md = text.replace(/```[\s\S]*?```/g, (m) => m.replace(/\n/g, "\n "))
+    md = newText.replace(/```[\s\S]*?```/g, (m) => m.replace(/\n/g, "\n "))
     md = md.replace(/(?<=\n\n)(?![*-])\n/g, "&nbsp;\n ")
     // Support single linebreak
     md = md.replace(/(\n)/gm, "  \n")
