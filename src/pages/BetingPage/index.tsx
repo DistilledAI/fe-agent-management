2b9b0e7b997b1e4ec53d1dc6f5812456226a9110
haitran <haitu.dev@gmail.com>
2024-12-04 16:32:24 +0700

    header bet

--- src/pages/BetingPage/index.tsx
+++ src/pages/BetingPage/index.tsx
@@ -1,7 +1,15 @@
+import HeaderBet from "./HeaderBet"
 import SwiperList from "./SwiperList"
 
 const Betting = () => {
-  return <SwiperList />
+  return (
+    <div>
+      <div className="mx-auto mb-10 max-w-[1200px] px-4">
+        <HeaderBet />
+      </div>
+      <SwiperList />
+    </div>
+  )
 }
 
 export default Betting
