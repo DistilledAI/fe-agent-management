3d3e01c8aae68998b19190c401d778cdd4102169
haitran <haitu.dev@gmail.com>
2024-12-19 14:53:21 +0700

    update style and text

--- src/pages/ChatPage/ChatBox/LeftBar/AnalyticsInfoWrap.tsx
+++ src/pages/ChatPage/ChatBox/LeftBar/AnalyticsInfoWrap.tsx
@@ -10,7 +10,7 @@ const AnalyticsInfoWrap: React.FC = () => {
   return (
     <div
       className={twMerge(
-        "flex items-center justify-between gap-4 py-2 transition-all duration-300 ease-in-out",
+        "relative z-[1] flex items-center justify-between gap-4 py-2 transition-all duration-300 ease-in-out",
         sidebarCollapsed && "flex-col",
       )}
     >
